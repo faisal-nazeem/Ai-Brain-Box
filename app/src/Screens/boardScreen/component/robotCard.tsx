5b65466cc32930d0dbba609208a81820755fc616
faisal-nazeem <faisalhunzai554@.com>
2025-03-17 19:36:06 +0500

    change a card

--- app/src/Screens/boardScreen/component/robotCard.tsx
+++ app/src/Screens/boardScreen/component/robotCard.tsx
@@ -1,16 +1,47 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, ImageSourcePropType } from 'react-native'
 import React from 'react'
+import Label from '@/app/src/component/label';
 
-export default function robotCard() {
+interface RobotCardProps {
+  imageSource: ImageSourcePropType;
+  labelText: string;
+  onClickStyle?: object;
+  Text: string;
+}
+
+const RobotCard: React.FC<RobotCardProps> = ({ imageSource, labelText, onClickStyle, Text }) => {
   return (
     <View style={styles.container}>
-    <Image source={require('../../../../../assets/images/robot.png')} />
+      <View>
+        <Image source={imageSource} style={styles.image} />
+      </View>
+      <View style={[styles.click, onClickStyle]} />
+      <View style={styles.textContainer}>
+        <Label text={labelText} fontSize={33} fontWeight={"bold"} />
+        <Label text={Text} fontSize={16} fontWeight={'200'} />
+      </View>
     </View>
   )
 }
 
+export default RobotCard
+
 const styles = StyleSheet.create({
-    container:{
-        
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  click: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: 'black',
+  },
+  image: {
+    resizeMode: 'contain',
+  },
+  textContainer: {
+    alignSelf: 'center', 
+  },
 })
